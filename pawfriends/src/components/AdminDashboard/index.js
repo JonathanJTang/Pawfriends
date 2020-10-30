fa3520eda1a0a35b6760f344b93e47b5f7d0ccf1
Jonathan Tang <jonathanj.tang@mail.utoronto.ca>
2020-10-30 13:28:03 -0400

    Start Admin Dashboard

--- pawfriends/src/components/AdminDashboard/index.js
+++ pawfriends/src/components/AdminDashboard/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
+// import NavBar from '../NavBar';
 import "./styles.css";
+
+import logo from "./../../images/logo.png";
+import logout from "./../../images/logout.png";
 
 /* Admin dashboard component */
 class AdminDashboard extends React.Component {
   render() {
+    const {appState} = this.props;
+
     return (
-      <div>
-        Nothing added yet for this component.
+      <div className="main">
+        <div className="adminNavbar">
+          <img src={logo} alt='logo' className='logo' />
+          <div className="navbarTextButton">Admin Dashboard</div>
+          <img src={logout} alt='logout' className='logout' />
+        </div>
+        <div className="statsTableContainer">
+          <h2>Site Statistics</h2>
+          <table className="statsTable">
+            <tr className="statsTableRow">
+              <td className="statsTableLeftCell">Number of regular users</td>
+              <td className="statsTableRightCell">
+                {appState.users.reduce((total, user) => {
+                  return user.type === "user" ? total + 1 : total
+                }, 0)}</td>
+            </tr>
+            <tr className="statsTableRow">
+              <td className="statsTableLeftCell">Number of admin users</td>
+              <td className="statsTableRightCell">
+                {appState.users.reduce((total, user) => {
+                  return user.type === "admin" ? total + 1 : total
+                }, 0)}</td>
+            </tr>
+            <tr className="statsTableRow">
+              <td className="statsTableLeftCell">Total number of posts</td>
+              <td className="statsTableRightCell">
+                {appState.posts.length}</td>
+            </tr>
+          </table>
+        </div>
       </div>
     );
   }
