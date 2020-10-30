5b7f92c63c012db465d5b5524e21958b135f0f30
rosielly <72799083+rosielly@users.noreply.github.com>
2020-10-30 01:26:26 -0700

    Update UI for settings page

--- pawfriends/src/components/Settings/index.js
+++ pawfriends/src/components/Settings/index.js
@@ -14,16 +14,17 @@ class Settings extends React.Component {
     return (
       <div>
         <NavBar />
+
         <form className='settings-form'>
-          <label>
+          <label className='settings-label'>
             Email:
             <input type='text' value={this.state.email} className='settings-input' />
           </label>
-          <label>
+          <label className='settings-label'>
             Password:
             <input type='password' value={this.state.pass} className='settings-input' />
           </label>
-          <input type='submit' value='Save' className='settings-submit' />
+          <input type='submit' value='Change' className='settings-submit' />
         </form>
       </div>
     );
