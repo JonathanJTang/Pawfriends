34ba2f7b0f427d05c26c604e199a7044f9db4e94
rosielly <72799083+rosielly@users.noreply.github.com>
2020-10-29 19:13:28 -0700

    Make profile css modular

--- pawfriends/src/components/Profile/index.js
+++ pawfriends/src/components/Profile/index.js
@@ -1,67 +1,89 @@
 import React from 'react';
-import './styles.css';
+import styles from './profile.module.css';
 
 import logo from './logo.png';
 import logout from './logout.png';
 import avatar from './avatar.jpg';
 import photo from './photo.png';
 import heart from './heart.png';
+import edit from './edit.png'
+import post from './post.png'
 
 /* Profile component */
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { status: 'Staying cozy' }
+  }
+
+  changeStatus = () => {
+    this.setState({ status: 'Changed status' });
+  }
+
+  // edit = () => {
+
+  // }
+
+  // save = () => {
+  //   this.changeStatus(this.target.value);
+  // }
+
   render() {
     return (
       <div>
-        <div className = 'navbar'>
-          <img src = {logo} alt = 'logo' className = 'logo'/>
+        <div className={styles.navbar}>
+          <img src={logo} alt='logo' className={styles.logo} />
           <ul>
-            <li><a href = '.'>Home</a></li>
-            <li><a href = './profile'>Profile</a></li>
-            <li><a href = './settings'>Settings</a></li>
+            <li><a href='.'>Home</a></li>
+            <li><a href='./profile'>Profile</a></li>
+            <li><a href='./settings'>Settings</a></li>
           </ul>
-          <img src = {logout} alt = 'logout' className = 'logout'/>
+          <img src={logout} alt='logout' className={styles.logout} />
         </div>
 
-        <div className = 'profile'>
-          <div></div>
-          <p className = 'status'>Staying cozy</p>
+        <button className={styles.edit} onClick={this.changeStatus}><img src={edit} /></button>
+        <button className={styles.newPost}><img src={post} /></button>
 
-          <img src = {avatar} alt = 'avatar' className = 'avatar' />
+        <div className={styles.profile}>
+          <div></div>
+          <p className={styles.status}>{this.state.status}</p>
+
+          <img src={avatar} alt='avatar' className={styles.avatar} />
 
           <p><strong>Lulu</strong></p>
 
-          <p className = 'location'>Toronto, Canada</p>
+          <p className={styles.location}>Toronto, Canada</p>
         </div>
 
-        <div className = 'gallery'>
+        <div className={styles.gallery}>
           <h1>Photos</h1>
-          <img src = {photo} className = 'photo' />
-          <img src = {photo} className = 'photo' />
-          <img src = {photo} className = 'photo' />
-          <img src = {photo} className = 'photo' />
-          <img src = {photo} className = 'photo' />
-          <img src = {photo} className = 'photo' />
+          <img src={photo} className={styles.photo} />
+          <img src={photo} className={styles.photo} />
+          <img src={photo} className={styles.photo} />
+          <img src={photo} className={styles.photo} />
+          <img src={photo} className={styles.photo} />
+          <img src={photo} className={styles.photo} />
         </div>
 
-        <div className = 'posts'>
-          <div className = 'post'>
+        <div className={styles.posts}>
+          <div className={styles.post}>
             <h2>
               Perfect day for a walk
-              <img src = {heart} className = 'like' />
+              <img src={heart} className={styles.like} />
             </h2>
             <p>...</p>
           </div>
-          <div className = 'post'>
+          <div className={styles.post}>
             <h2>
               Perfect day for a walk
-              <img src = {heart} className = 'like' />
+              <img src={heart} className={styles.like} />
             </h2>
             <p>...</p>
           </div>
-          <div className = 'post'>
+          <div className={styles.post}>
             <h2>
               Perfect day for a walk
-              <img src = {heart} className = 'like' />
+              <img src={heart} className={styles.like} />
             </h2>
             <p>...</p>
           </div>
