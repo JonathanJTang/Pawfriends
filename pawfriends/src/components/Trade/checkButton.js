bdfeecde797c25ecefc08d8c8f92ba1fa1b72e9b
Jonathan Tang <jonathanj.tang@mail.utoronto.ca>
2023-02-14 18:54:59 -0500

    Add Popup for mark trade as done

--- pawfriends/src/components/Trade/checkButton.js
+++ pawfriends/src/components/Trade/checkButton.js
@@ -1,18 +1,33 @@
 import React from "react";
 
+import Popup from "../Popup";
 import check from "../../images/check.png";
 import checkFill from "../../images/check-fill.png";
 
 import { finishTrade } from "../../actions/apiRequests";
 
 class CheckButton extends React.Component {
-  handleClick = async () => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMarkDonePopup: false,
+    };
+  }
+
+  handleMarkDoneClick = () => {
+    this.state.showMarkDonePopup
+      ? this.setState({ showMarkDonePopup: false })
+      : this.setState({ showMarkDonePopup: true });
+  };
+
+  markTradeDone = async () => {
     // Can only mark trade as complete one time
     if (!this.props.trade.done) {
       const response = await finishTrade(this.props.trade._id);
       if (response !== undefined) {
         this.props.trade.done = true;
         this.props.stateUpdate(this.props.trade);
+        this.handleMarkDoneClick();
       }
     }
   };
@@ -20,10 +35,19 @@ class CheckButton extends React.Component {
   render() {
     return (
       <div className="trade-bar-btn">
+        {this.state.showMarkDonePopup && (
+          <Popup
+            title="Confirm Mark Trade as Done"
+            content=""
+            confirm={this.markTradeDone}
+            cancel={this.handleMarkDoneClick}
+          />
+        )}
         <img
           src={this.props.trade.done ? checkFill : check}
-          onClick={this.handleClick}
           alt={this.props.trade.done ? "trade done" : "trade not done"}
+          onClick={this.handleMarkDoneClick}
+          className={this.props.trade.done ? "trade-done" : ""}
         />
       </div>
     );
